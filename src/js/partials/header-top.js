662ade4d5aeb9cc8e6aa778fc928e01788ce5cb8
Artem B <a.brovkin@regiomedia.ru>
2019-08-20 17:44:51 +0300

    Style language block

--- src/js/partials/header-top.js
+++ src/js/partials/header-top.js
@@ -1,3 +1,15 @@
 $('.header-top__lang-icon').on('click', function(){
+    $('.header-top__lang-text-block').css({display: 'flex'});
+});
 
+
+
+
+$(document).click(function (e){ // событие клика по веб-документу
+    var div = $(".header-top__lang-block"); // тут указываем ID элемента
+    var languages = $('.header-top__lang-text-block')
+    if (!div.is(e.target) // если клик был не по нашему блоку
+        && div.has(e.target).length === 0) { // и не по его дочерним элементам
+        languages.hide(); // скрываем его
+    }
 });
