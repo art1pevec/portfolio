5dfca96b153562b4076b28c4605b991494d982de
art1pevec <brovkin.on@gmail.com>
2019-08-21 20:50:16 +0300

    trying slider in slider (not working)

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 @@include('../../bower_components/jquery/dist/jquery.js')
 @@include('../../bower_components/slick-carousel/slick/slick.js')
 @@include('../../bower_components/vue/dist/vue.js')
+@@include('../../bower_components/owl.carousel/dist/owl.carousel.js')
 
 @@include('partials/app.js')
 @@include('partials/main-menu.js')
 @@include('partials/header-top.js')
 @@include('partials/main-slider.js')
+@@include('partials/main-screen-slider.js')
 
 
 
