99e92ff4f9fd5ce76a2797821310244a1d5d0e69
Artem B <a.brovkin@regiomedia.ru>
2019-08-23 10:52:56 +0300

    Update left-menu and button in main-screen

--- src/js/partials/main-menu.js
+++ src/js/partials/main-menu.js
@@ -7,22 +7,29 @@ $(document).ready(function(){
 
         if ($('.left-menu').hasClass('active')) {
             $('.left-menu').css('display', 'block')
-                .animate({width: '300px'});
+                .animate({width: '270px'});
         } else {
             $('.left-menu').animate({width: '0px'}, function(){
                 $(this).css('display', 'none');
             })
 
         }
+            if ($(this).hasClass('active')) {
+                $('.left-menu__icon').removeClass('active');
+            //     $('.left-menu__line').removeClass('active');
+            //     $('.left-menu__icon').next().removeClass('active');
+            }
 
-        $('.left-menu__icon').removeClass('active');
-        $('.left-menu__icon').next().removeClass('active');
     });
 
 
 
     $('.left-menu__icon').on('click', function(){
+        if ($(this).hasClass('active')) {
+            return false;
+        }
+        // $('.left-menu__line').toggleClass('active');
        $(this).toggleClass('active');
-       $(this).next().toggleClass('active');
+       // $(this).next().toggleClass('active');
     });
 });
