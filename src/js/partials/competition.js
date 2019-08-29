3d1a85a3b259e204a569e08807d621ddb0b88f68
art1pevec <brovkin.on@gmail.com>
2019-08-29 07:55:12 +0300

    Competitions flex and hide script

--- src/js/partials/competition.js
+++ src/js/partials/competition.js
@@ -1,5 +1,5 @@
-var htmlSTring = '<div class="competition-screen__item">\
-                         <div @click="clickItem" class="competition-screen__item-title">{{ item.title }}</div>\
+var htmlSTring = '<div @click="clickItem" class="competition-screen__item">\
+                         <div class="competition-screen__item-title">{{ item.title }}</div>\
                           <transition name="slide-fade">\
                          <div v-if="item.visible" class="competition-screen__item-more">{{ item.message }}</div>\
                          </transition>\
@@ -9,6 +9,15 @@ Vue.component('competitions', {
     props: ['item'],
     methods: {
       clickItem: function() {
+          var arr = main.competitions;
+
+          arr.map(function(el){
+             if (el.visible == true) {
+                 el.visible = false;
+             }
+             console.log(el);
+          });
+
           this.item.visible = !this.item.visible;
       }
     },
