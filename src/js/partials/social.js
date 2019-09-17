215c818affa54f08e949ebdf2f717900b533deb4
Artem B <a.brovkin@regiomedia.ru>
2019-09-17 14:52:13 +0300

    reload fixes разбор

--- src/js/partials/social.js
+++ src/js/partials/social.js
@@ -1,5 +1,14 @@
-$('.social-icon').click(function(){
-    $(this).find('.social-icon__circle').toggleClass('circle-animation');
-    $(this).toggleClass('active');
-    $(this).toggleClass('not-active');
+$(document).ready(function(){
+
+    $('.social-icon__icon-block').click(function(){
+        // $(this).find('.social-icon__circle').toggleClass('circle-animation');
+        // $(this).toggleClass('active');
+        // $(this).toggleClass('not-active');
+        $(this).toggleClass('active');
+        $('.social-icon__contacts-block').toggleClass('active');
+
+        $('.social-icon__icon').toggleClass('fa-plus');
+        $('.social-icon__icon').toggleClass('fa-times');
+    });
+
 });
